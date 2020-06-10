6b3df702f309a160726ee5edd9f397e1067674a2
liushiquan <liushiquan@meicai.cn>
2020-06-10 10:41:47 +0800

    init

--- src/main/bin/stop.bat
+++ src/main/bin/stop.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem 启动类名
+rem Bootstrap main class
 set "MAIN_CLASS=App"
 for /f "tokens=1" %%a in ('jps ^| findstr %MAIN_CLASS%') do taskkill /f /pid %%a
